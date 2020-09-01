d8eec595c1e6a828d9d91c9f5f94bacd3f65ed99
deatil <2217957370@qq.com>
2020-09-01 12:38:42 +0800

    更新

--- src/lake/admin/provider.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use lake\admin\boot\ExceptionHandle;
-
-// 容器Provider定义文件
-return [
-    'think\exception\Handle' => ExceptionHandle::class,
-];
--- src/lake/lake/config/lake.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "name" => "LakeAdmin",
-    "name_mini" => "Lake",
-    "release" => 20200825,
-    "version" => "2.3.1.20200825",
-];
