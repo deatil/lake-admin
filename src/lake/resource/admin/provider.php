7acc0844d1360f82dc72f4e12aaa2a1d56f424b2
deatil <2217957370@qq.com>
2021-02-04 23:13:51 +0800

    thinkphp升级到6.0.7

--- src/lake/resource/admin/provider.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Lake\Admin\Exception\ExceptionHandle;
-
-// 容器Provider定义文件
-return [
-    'think\exception\Handle' => ExceptionHandle::class,
-];
--- src/lake/resource/config/lake.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "name" => "LakeAdmin",
-    "name_mini" => "Lake",
-    "release" => "2.3.29.20210205",
-    "version" => "2.3.29",
-];
